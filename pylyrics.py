0965424c1db6b71cac2f96745c9b71c2d41a8974
BugsForDays <jmslca123@gmail.com>
2018-06-12 18:38:07 -0700

    cleaned up

--- pylyrics.py
+++ pylyrics.py
@@ -1,11 +1,5 @@
 import requests
 from bs4 import BeautifulSoup
-#
-# spotify_req = requests.get('https://open.spotify.com')
-# soup = BeautifulSoup(spotify_req.content, "html.parser")
-# print(spotify_req.content)
-# print(soup.title.string)
-
 import ctypes
 
 EnumWindows = ctypes.windll.user32.EnumWindows
@@ -23,34 +17,41 @@ def foreach_window(hwnd, lParam):
         titles.append(buff.value)
     return True
 EnumWindows(EnumWindowsProc(foreach_window), 0)
-
-# all code above this line is not my code
-
+#code above not my code
 def get_song_info():
     for item in titles:
         if '-' in item and 'Google' not in item and 'python' not in item and 'cmd' not in item:
             return item
 
-song_string= get_song_info().split(' - ')
-artist = song_string[0]
-song = song_string[1]
-print(artist)
-print(song)
 def create_search_query(artist, song):
     new_artist_string = artist.replace(' ', '+')
     new_song_string = song.replace(' ', '+')
-    # print(new_artist_string)
     return new_song_string + '%20' + new_artist_string
-# create_search_query(artist,song)
-search_url = 'https://genius.com/api/search/multi?per_page=2&q=' + create_search_query(artist,song)
-print(search_url)
-search_request = requests.get(search_url)
-lyrics_url = search_request.json()['response']['sections'][0]['hits'][0]['result']['url']
-# print(search_request.content)
-# search_soup = BeautifulSoup(search_request.content, 'html.parser')
-# print(search_soup.findAll('a'))
 
-lyrics_html = requests.get(lyrics_url)
-soup = BeautifulSoup(lyrics_html.content, "html.parser")
-lyrics = soup.find('div', 'lyrics').get_text()
-print(lyrics)
+def create_lyrics_url(search_query):
+    search_url = 'https://genius.com/api/search/multi?per_page=2&q=' + search_query
+    # print(search_url)
+    search_request = requests.get(search_url)
+    lyrics_url = search_request.json()['response']['sections'][0]['hits'][0]['result']['url']
+    return lyrics_url
+
+def get_lyrics(url):
+    lyrics_html = requests.get(url)
+    soup = BeautifulSoup(lyrics_html.content, "html.parser")
+    lyrics = soup.find('div', 'lyrics').get_text()
+    return lyrics
+
+if __name__ == '__main__':
+    song_string = get_song_info().split(' - ')
+    artist = song_string[0]
+    song = song_string[1]
+    query = create_search_query(artist,song)
+    lyrics_url = create_lyrics_url(query)
+    lyrics = get_lyrics(lyrics_url)
+    print('='*10)
+    print('PYLYRICS')
+    print('='*10)
+    print('Finding lyrics for: ' + song + ' by ' + artist)
+    print('Searching @ ' + lyrics_url)
+    print('LYRICS: ')
+    print(lyrics)
